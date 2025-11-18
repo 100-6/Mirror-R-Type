dceef1d0cf6091e514b67e07a733f566a045b6eb
ouioui <adam.larkem@epitech.eu>
2025-11-18 18:18:01 +0100

    fix(Build): Now building in a already builded environement clean it and build again

--- build.sh
+++ build.sh
@@ -10,7 +10,13 @@ if [ "$1" == "clean" ]; then
     exit 0
 fi
 
+if [ -d "$BUILD_DIR" ] && [ -f "$BUILD_DIR/CMakeCache.txt" ]; then
+    echo "Build directory detected. Cleaning and rebuilding..."
+    rm -rf "$BUILD_DIR"
+fi
+
+echo "Configuring CMake..."
 cmake -S . -B "$BUILD_DIR"
 
-echo "Building"
+echo "Building..."
 cmake --build "$BUILD_DIR"
